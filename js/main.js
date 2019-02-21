3bdf8b042d684389dc7b523ac89a72c2c68d636c
bharatchauhan51 <41588655+bharatchauhan51@users.noreply.github.com>
2019-02-21 22:10:12 +0530

    OtherWeatherAPI

--- js/main.js
+++ js/main.js
@@ -7,26 +7,33 @@ window.addEventListener('load',()=>{
     );
     let temperatureDegree = document.querySelector(".temperature-degree");
     let locationTimezone = document.querySelector(".location-timezone");
+    
+    
     if(navigator.geolocation){
         navigator.geolocation.getCurrentPosition(position =>{
            // console.log(position);
-            long=position.coords.longitude;
+            lon=position.coords.longitude;
             lat=position.coords.latitude;
             const proxy='https://cors-anywhere.herokuapp.com/';
-            const api=`${proxy}https://api.darksky.net/forecast/aa574989991652854b1188960af4f62f/${lat},${long}`;
+            console.log(lat);
+            const api=`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}`;
             console.log();
-            fetch(api)
+            fetch(`https://api.openweathermap.org/data/2.5/weather?APPID=83e379c14211a6faba49c4fae33a9e25&lat=${lat}&lon=${lon}`)
                 .then(response=>{
-                     //console.log(response);
+                     
                      return response.json();
+                     
                 })
                  .then(data=>{
                       let c;
-                      const{temperature,summary}=data.currently;
-                      c=((temperature-32)*5/9).toFixed(2);
+                      console.log(data);
+                      const{temp}=data.main;
+                      const{description}=data.weather[0];
+                      c=(temp-273.15).toFixed(2);
                       temperatureDegree.textContent= c;
-                      temperatureDescription.textContent= summary;
-                      console.log(summary);
+                      temperatureDescription.textContent= description;
+                      locationTimezone.textContent=data.name;
+                      console.log(description);
                 });
         });
     }
